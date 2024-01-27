736e8e67f1bda981294f1f4a793c1d806e15b384
rajibsutradhartonmoy <rajibsutradhartonmoy@gmail.com>
2024-01-27 12:19:56 +0600

    removed subscription

--- src/services/isUserPremium.js
+++ src/services/isUserPremium.js
@@ -1,15 +1,11 @@
 import { getAuth } from "firebase/auth";
-import {
-  getFirestore,
-  addDoc,
-  collection,
-  onSnapshot,
-} from "firebase/firestore";
 
 export const isUserPremium = async () => {
   const auth = getAuth();
   await auth.currentUser?.getIdToken(true);
   const decodedToken = await auth.currentUser?.getIdTokenResult();
   console.log("decoded Token", decodedToken);
-  return decodedToken?.claims?.stripeRole ? true : false;
+  // return decodedToken?.claims?.stripeRole ? true : false; // TODO: commented for testing
+
+  return true;
 };
